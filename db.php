0f831ad8983d9e47d5d17e53083d709ca495dcdd
SenseiSaku <kumaandloli@gmail.com>
2024-11-12 09:13:43 +0700

    database

--- db.php
+++ db.php
@@ -8,6 +8,6 @@ try{
     $conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
     echo "success";
 }catch (PDOException $e){
-    echo "fail" . $e->getMessage();
+    echo "faila" . $e->getMessage();
 }
 ?>
